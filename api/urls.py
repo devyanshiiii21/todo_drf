59c8a30864742b5e7bffc071e46747a1da3ce8ae
Devyanshi Srivastava <devyanshi2106@gmail.com>
2023-04-16 13:09:31 +0530

    Posted Tasks

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.apiOverview, name = 'api-overview')
+    path('', views.apiOverview, name = 'api-overview'),
+    path('task-list/', views.taskList, name = 'api-tasklist'),
+    path('task-details/<str:pk>/', views.taskDetail, name = 'api-taskdetail'),
 ]
