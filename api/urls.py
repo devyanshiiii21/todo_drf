af6062d880374d91d20ca0fbd11854b96c42c6e2
Devyanshi Srivastava <devyanshi2106@gmail.com>
2023-04-29 18:32:26 +0530

    Fixed a URL error

--- api/urls.py
+++ api/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.apiOverview, name = 'api-overview'),
     path('task-list/', views.taskList, name = 'task-list'),
     path('task-details/<str:pk>/', views.taskDetail, name = 'task-detail'),
-    path('task-create', views.taskCreate, name = 'task-create'),
+    path('task-create/', views.taskCreate, name = 'task-create'),
     path('task-update/<str:pk>', views.taskUpdate, name = 'task-update'),
     path('task-delete/<str:pk>', views.taskDelete, name = 'task-delete'),
 ]
